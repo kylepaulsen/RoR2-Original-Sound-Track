a26353a8c7dc1ad7fffef19fe8d3715aa90692ca
Kyle Paulsen <kyle.a.paulsen@gmail.com>
2020-02-11 21:38:39 -0800

    Init commit.

---
+++ ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,273 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Reflection;
+
+namespace OriginalSoundTrack {
+    // The OriginalSoundTrack plugin - For replacing the in game music with Risk of Rain 1 music (or your own).
+    // You will need access to your own risk of rain 1 sound files (or any others you want to use).
+    // Edit the settings.xml to specify how the music plays in game.
+    // Also don't forget to mute the in game music in the in game settings (this plugin doesn't take away RoR2 music).
+
+    //This attribute specifies that we have a dependency on R2API, as we're using it to add Bandit to the game.
+    //You don't need this if you're not using R2API in your plugin, it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency("com.bepis.r2api")]
+
+    //This attribute is required, and lists metadata for your plugin.
+    //The GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config). I like to use the java package notation, which is "com.[your name here].[your plugin name here]"
+    //The name is the name of the plugin that's displayed on load, and the version number just specifies what version the plugin is.
+    [BepInPlugin("com.kylepaulsen.originalsoundtrack", "OriginalSoundTrack", "1.0")]
+
+    //This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class OriginalSoundTrack : BaseUnityPlugin {
+
+        private FadeInOutSampleProvider fader;
+        private WaveOutEvent outputDevice;
+
+        private FileInfo[] soundFiles; // array of files found in the plugin folder
+        private List<Music> musics = new List<Music>(); // list of main music objects.
+        private AudioFileReader currentSong;
+        private string currentSongFullName = ""; // helpful for not restarting a song when it's already playing.
+        private DateTime lastPlayTime; // helps prevent multiple calls to PlayMusic in a row.
+        private bool startedTeleporterEvent = false; // tracks the first interaction with the tele.
+        private bool songPaused = false; // for pausing the music when the player pauses.
+        private float globalMusicVolume = 0.5f; // default global music volume.
+        private bool shouldLoop = true; // should songs loop when they end?
+        private string oldMusicVolume = ""; // what the music convar was before we override it.
+        private string currentScene = ""; // helpful for picking out boss music.
+        private System.Random rnd = new System.Random(); // helpful for picking random music.
+        private XmlElement settings; // settings data.
+
+        //The Awake() method is run at the very start when the game is initialized.
+        public void Awake() {
+            var pluginPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var info = new DirectoryInfo(pluginPath);
+            soundFiles = info.GetFiles()
+                .Where(f => System.IO.Path.GetExtension(f.Name) == ".mp3" || System.IO.Path.GetExtension(f.Name) == ".wav")
+                .ToArray();
+
+            try {
+                var settingsXml = new XmlDocument();
+                settingsXml.Load(pluginPath + "/settings.xml");
+                settings = settingsXml["settings"];
+
+                globalMusicVolume = float.Parse(settings["volume"].InnerText);
+                shouldLoop = settings["loop"].InnerText.ToLower() == "true";
+
+                foreach (XmlNode node in settings["music"].ChildNodes) {
+                    if (node.NodeType != XmlNodeType.Comment) {
+                        var newMusic = new Music();
+                        newMusic.name = GetAttribute(node, "name");
+                        newMusic.scenes = GetAttribute(node, "scenes").Split(',').Select(str => str.Trim()).ToArray();
+                        newMusic.boss = GetAttribute(node, "boss").ToLower() == "true";
+                        newMusic.volume = 1f;
+                        var vol = GetAttribute(node, "volume");
+                        if (vol != "") {
+                            newMusic.volume = float.Parse(vol);
+                        }
+
+                        foreach (var soundFile in soundFiles) {
+                            if (soundFile.Name == newMusic.name) {
+                                newMusic.fullName = soundFile.FullName;
+                                break;
+                            }
+                        }
+
+                        musics.Add(newMusic);
+                    }
+                }
+            } catch (Exception ex) {
+                Debug.Log("!!!!! OriginalSoundTrack Mod: Failed to parse settings.xml !!!!!");
+                Debug.Log("OriginalSoundTrack Mod: Music will be randomly selected from what is found in the plugin dir.");
+                Debug.Log(ex);
+            }
+
+            if (outputDevice == null) {
+                outputDevice = new WaveOutEvent();
+            }
+
+            On.RoR2.TeleporterInteraction.OnInteractionBegin += (orig, self, activator) => {
+                orig(self, activator);
+                if (!startedTeleporterEvent) {
+                    startedTeleporterEvent = true;
+                    PickOutMusic(true);
+                }
+            };
+
+            On.RoR2.UI.PauseScreenController.OnEnable += (orig, self) => {
+                orig(self);
+                if (outputDevice != null && outputDevice.PlaybackState == PlaybackState.Playing) {
+                    outputDevice.Pause();
+                    songPaused = true;
+                }
+            };
+
+            On.RoR2.UI.PauseScreenController.OnDisable += (orig, self) => {
+                orig(self);
+                if (outputDevice != null && songPaused == true) {
+                    outputDevice.Play();
+                    songPaused = false;
+                }
+            };
+
+            SceneManager.sceneLoaded += (scene, mode) => {
+                currentScene = scene.name;
+                #if DEBUG
+                    Debug.Log("====================== CHANGE SCENE ========================");
+                    Debug.Log(currentScene);
+                #endif
+
+                startedTeleporterEvent = false;
+
+                PickOutMusic();
+            };
+        }
+
+        private string GetAttribute(XmlNode node, string attribute) {
+            if (node.Attributes != null) {
+                var attr = node.Attributes.GetNamedItem(attribute);
+                if (attr != null) {
+                    return attr.Value;
+                }
+            }
+            return "";
+        }
+
+        private bool sceneMostlyMatches(string[] scenes) {
+            // this handles scenes that can have numbers on the end.
+            foreach (var scene in scenes) {
+                if (currentScene.Contains(scene)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void PickOutMusic(bool isForTeleporter = false) {
+            var goodMusicChoices = musics.Where(music => {
+                var bossTest = isForTeleporter == music.boss;
+                return music.fullName != null && bossTest && sceneMostlyMatches(music.scenes);
+            }).ToArray();
+
+            #if DEBUG
+                Debug.Log("====== Selecting Music ======");
+                Debug.Log("Current Scene: " + currentScene);
+                Debug.Log("Is For Teleporter: " + isForTeleporter);
+                Debug.Log("Choices: ");
+                foreach(var choice in goodMusicChoices) {
+                    Debug.Log(choice.name);
+                }
+                Debug.Log("");
+            #endif
+
+            var randFile = currentSongFullName;
+            Music randMusic = null;
+            int tries = 0;
+
+            if (goodMusicChoices.Length > 0) {
+                while (randFile == currentSongFullName && tries < 10) {
+                    randMusic = goodMusicChoices[rnd.Next(goodMusicChoices.Length)];
+                    randFile = randMusic.fullName;
+                    tries++;
+                }
+                StartCoroutine(PlayMusic(randFile, randMusic.volume));
+                return;
+            }
+
+            #if DEBUG
+                Debug.Log("======= MUSIC PICK FAILED! =======");
+                Debug.Log("choosing random song...");
+            #endif
+            // if we are here, then we failed to pick a music, so pick one at random from all of them we found.
+            tries = 0;
+            while (randFile == currentSongFullName && tries < 10) {
+                randFile = soundFiles[rnd.Next(soundFiles.Length)].FullName;
+                tries++;
+            }
+            StartCoroutine(PlayMusic(randFile));
+        }
+
+        private IEnumerator<WaitForSeconds> PlayMusic(string file, float volume = 1f) {
+            if (file != currentSongFullName) {
+                if (lastPlayTime == null || (DateTime.UtcNow - lastPlayTime).TotalSeconds > 5) {
+                    lastPlayTime = DateTime.UtcNow;
+                    if (outputDevice.PlaybackState == PlaybackState.Playing) {
+                        fader.BeginFadeOut(1500);
+                        yield return new WaitForSeconds(1.5f);
+                        outputDevice.Stop();
+                        currentSong = null;
+                    }
+                    currentSongFullName = file;
+                    currentSong = new AudioFileReader(file);
+                    currentSong.Volume = volume * globalMusicVolume;
+                    fader = new FadeInOutSampleProvider(currentSong);
+                    outputDevice.Init(fader);
+                    #if DEBUG
+                        Debug.Log("====== Now Playing: " + file);
+                    #endif
+                    outputDevice.Play();
+                    songPaused = false;
+                } else {
+                    #if DEBUG
+                        Debug.Log("PlayMusic: Tried to play another song too soon!!! " + file);
+                    #endif
+                }
+            } else {
+                #if DEBUG
+                    Debug.Log("PlayMusic: Already playing: " + file);
+                #endif
+            }
+        }
+
+        public void Update() {
+            if (oldMusicVolume == "" && RoR2.Console.instance != null) {
+                var convar = RoR2.Console.instance.FindConVar("volume_msx");
+                // set in game music volume to 0 so we hear the new music only.
+                if (convar != null) {
+                    oldMusicVolume = convar.GetString();
+                    convar.SetString("0");
+                }
+            }
+        }
+
+        private void FixedUpdate() {
+            if (currentSong != null) {
+                if (currentSong.Position >= currentSong.Length) {
+                    if (shouldLoop) {
+                        currentSong.Position = 0;
+                    } else {
+                        outputDevice.Stop();
+                        currentSong.Position = currentSong.Length - 1;
+                        PickOutMusic(startedTeleporterEvent);
+                    }
+                }
+            }
+        }
+
+        private void OnDestroy() {
+            var convar = RoR2.Console.instance.FindConVar("volume_msx");
+            if (convar != null) {
+                convar.SetString(oldMusicVolume);
+            }
+        }
+    }
+
+    public class Music {
+        public string name;
+        public string fullName;
+        public string[] scenes;
+        public bool boss = false;
+        public float volume = 1f;
+    }
+}
---
+++ README.md
@@ -0,0 +1,93 @@
+# The OriginalSoundTrack Plugin
+
+This mod allows you to play Risk of Rain 1 (or any) music in place of the normal soundtrack.
+
+You must supply your own music files though.
+
+Place the DLLs, settings.xml and your music files in:
+Risk of Rain 2\BepInEx\plugins\OriginalSoundTrack
+
+You must supply your own copy of the Risk of Rain 1 soundtrack by placing those files in the same folder as above.
+Files can be in .mp3 or .wav format (with those exact extensions).
+
+Make sure the filenames match what's in settings.xml (or edit settings.xml to match your files.)
+
+## settings.xml
+
+If your files match all the song "names", then you dont need to do anything else, but feel free to customize.
+
+If the plugin fails to match a song for a scene, it will just choose a random one it found in the plugin directory.
+
+Put scene IDs in the "scenes" attribute to have audio play on that scene in settings.xml.
+The scenes attribute is comma separated. A scene only has to be "contained" in the real scene ID for it to match:
+("golemplains" in the scene attr will match for the golemplains2 scene id.)
+
+Example for playing MyFile.mp3 on the title screen and main menus:
+```
+<song
+    name="MyFile.mp3"
+    scenes="title,lobby,logbook"
+    boss="false"
+    volume="1"
+/>
+```
+
+Setting boss to "true" means that music can only play when the player activates the teleport on a level.
+Otherwise music can only play elsewhere (non teleport events).
+"volume" is a decimal between 0 and 1.
+
+It's ok to have multiple `<song>` definitions with the same name or scenes. The plugin will choose a song randomly
+among the matching songs.
+
+The top level `<volume>` tag is the master volume for all this plugin's music. Again, a decimal between 0 and 1.
+
+The top level `<loop>` tag determines if music should loop or pick another song (from matching songs) after a song ends.
+
+## Scene IDs (level IDs).
+
+(As of the hidden realms update)
+
+See here for pictures of the level scenes:
+https://riskofrain2.fandom.com/wiki/Environments
+
+```
+Non Levels:
+
+ID                 Description
+===============================================
+title          -   the title screen
+lobby          -   the select character screen.
+logbook        -   view logs screen.
+
+Levels:
+
+ID                 Level Name / Description
+===============================================
+bazaar         -   Bazaar Between Time (The Shop)
+blackbeach     -   Distant Roost
+golemplains    -   Titanic Plains
+foggyswamp     -   Wetland Aspect
+goolake        -   Abandoned Aqueduct
+frozenwall     -   Rallypoint Delta
+wispgraveyard  -   Scorched Acres
+dampcavesimple -   Abyssal Depths
+shipgraveyard  -   Siren's Call
+
+arena          -   Void Fields (poison area with lots of "teleport" events.)
+mysteryspace   -   A Moment, Fractured (the place where you can obliterate yourself.)
+limbo          -   A Moment, Whole (the mega scavenger boss)
+goldshores     -   Glided Coast (the place where you fight the golden Stone Titan)
+```
+
+## Other Info
+
+This plugin uses NAudio.dll https://github.com/naudio/NAudio to help load and play music at runtime.
+This is done to make it very easy for players to supply their own music. The downside is that the normal in game
+music isn't disabled (Maybe there is a way to do that, I dunno). To get around this, this plugin sets your in game
+music volume setting to 0. It tries to set it back to where it was when the game closes but I'm not sure if this works.
+Anmyway you have been warned.
+
+## Licence
+
+When applicable, assume stuff is under the MIT licence ( https://opensource.org/licenses/MIT )
+I am not liable for any damages and there is no warranty etc...
