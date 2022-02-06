46b652d710526626c55f0be9360120da9a7ee322
Theray070696 <theray070696@gmail.com>
2022-02-05 21:53:42 -0500

    Make mod client side
    Requires R2API with NetworkCompatibility attribute

--- OriginalSoundTrack/OriginalSoundTrack.cs
+++ OriginalSoundTrack/OriginalSoundTrack.cs
@@ -1,5 +1,6 @@
-﻿using BepInEx;
+using BepInEx;
 using RoR2;
+using R2API.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -28,6 +29,8 @@ namespace OriginalSoundTrack {
     //The GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config). I like to use the java package notation, which is "com.[your name here].[your plugin name here]"
     //The name is the name of the plugin that's displayed on load, and the version number just specifies what version the plugin is.
     [BepInPlugin("com.kylepaulsen.originalsoundtrack", "OriginalSoundTrack", "1.2.0")]
+
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
 
     //This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
     //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
